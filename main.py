15c58ddc1e81bd9760dc23592f73d6f655bf9ab4
samzolin <saz5193@psu.edu>
2020-09-04 20:00:54 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+# Author: Samantha Zolin saz5193@psu.edu 
+lettergrade1 = input("Enter your course 1 letter grade: ")
+credit1 = input("Enter your course credit: ")
+credit1 = float(credit1)
+if lettergrade1 == "A":
+  gradepoint1 = 4.0 
+  print("Grade point for course 1 is: 4.0")
+elif letter == "A-":
+   gradepoint1 = 3.67
+  print("Grade point for course 1 is: 3.67")
+elif letter == "B+": 
+   gradepoint1 = 3.33
+  print("Grade point for course 1 is: 3.33")
+elif letter == "B": 
+   gradepoint1 = 3.0
+  print("Grade point for course 1 is: 3.0")
+elif letter == "B-":
+   gradepoint1 = 2.67
+  print("Grade point for course 1 is: 2.67")
+elif letter == "C+":
+   gradepoint1 = 2.33
+  print("Grade point for course 1 is: 2.33")
+elif letter == "C":
+   gradepoint1 = 2.0
+  print("Grade point for course 1 is: 2.0")
+elif letter == "D":
+   gradepoint1 = 1.0
+  print("Grade point for course 1 is: 1.0")
+elif letter == "F": 
+   gradepoint1 = 0.0
+  print("Grade point for course 1 is: 0.0")
+else:
+  gradepoint1 = 0.0
+  print("Grade point for course 1 is: 0.0")
+ 
+
+
